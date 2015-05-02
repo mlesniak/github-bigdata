642d6bb9ad4dc1453080e422e2f268efb2770f74
Michael Lesniak <mlesniak@micromata.de>
2015-05-02 23:49:30 +0200

    Example repository listing for 3pm on 2015-01-15.

--- src/main/java/com/micromata/bigdata/github/Main.java
+++ src/main/java/com/micromata/bigdata/github/Main.java
@@ -2,6 +2,8 @@ package com.micromata.bigdata.github;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +29,19 @@ public class Main {
   private void run(String[] args) {
     LOG.debug("SparkContext: {}", sc);
     LOG.debug("SQLContext: {}", sql);
+
+    DataFrame frame = sql.jsonFile("data/raw/2015-01-01-15.json.gz");
+    long count = frame.count();
+    LOG.debug("count={}", count);
+
+    sql.registerDataFrameAsTable(frame, "github");
+    DataFrame repos = this.sql.sql("SELECT DISTINCT repo.name from github");
+
+    for (Row row : repos.collectAsList()) {
+      LOG.info(row.getString(0));
+    }
+
+    LOG.trace("debug breakpoint");
   }
 
   public void initializeSpark() {

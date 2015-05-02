05ef9245f3f86c60dbc4213a544e192e016e4e0c
Michael Lesniak <mlesniak@micromata.de>
2015-05-02 19:25:08 +0200

    Spark dependencies and context.

--- src/main/java/com/micromata/bigdata/github/Main.java
+++ src/main/java/com/micromata/bigdata/github/Main.java
@@ -1,7 +1,35 @@
 package com.micromata.bigdata.github;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
+ * Entry point.
+ *
  * @author Michael Lesniak (mlesniak@micromata.de)
  */
 public class Main {
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+  private JavaSparkContext sc;
+
+  public Main() {
+    initializeSpark();
+  }
+
+  public static void main(String[] args) {
+    new Main().run(args);
+  }
+
+  private void run(String[] args) {
+    LOG.debug("SparkContext: {}", sc);
+  }
+
+  public void initializeSpark() {
+    SparkConf conf = new SparkConf()
+        .setAppName("Github conversion example.")
+        .setMaster("local[*]");
+    sc = new JavaSparkContext(conf);
+  }
 }
